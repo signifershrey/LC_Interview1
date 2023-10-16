3618f5c90d40f7802175c7a8dbcb5d0abe6a55df
Shrey Maurya <shreymaurya2000@gmail.com>
2023-10-16 18:00:36 +0530

    First-Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+import Page3 from "./Page3";
 
 function App() {
+
+  const [currentPage , setCurrentPage] = useState(1);
+  const totalPages=3;
+
+  const handlePrevClick = () => {
+    if(currentPage > 1){
+      setCurrentPage(currentPage -1);
+    }
+  }
+
+const handleNextClick = () => {
+  if (currentPage < totalPages) {
+    setCurrentPage(currentPage + 1);
+  }
+};
+
+
+const renderPage= () => {
+  switch (currentPage) {
+    case 1:
+      return <Page1 />;
+    case 2:
+      return <Page2 />;
+    case 3:
+      return <Page3 />;
+    default:
+      return null;
+  }
+}  
+
+const array = [1,2,3];
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex items-center justify-center h-screen flex-col bg-slate-500 w-full ">
+      <div className=" pt-10 text-center bg-purple-100 w-[60%] h-[40%] rounded-lg mb-4">
+        {
+          currentPage  === 1 ? <Page1/> : renderPage(currentPage) 
+        }
+      </div>
+
+      <div className="flex w-[60%] justify-around text-white">
+        <button
+          className=" bg-slate-900 px-2 py-1  rounded-md"
+          onClick={handlePrevClick}
+          disabled={currentPage === 1}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </button>
+
+        <ul className="flex px-4 py-1 justify-around ">
+          {array.map((item,index) => (
+            <li className={index+1===currentPage? 'bg-black px-2 rounded-md' : 'px-4'} key={index}>{item}
+            </li>
+          ))}
+        </ul>
+
+        <button
+          className=" bg-slate-900 px-2 py-1  rounded-md"
+          onClick={handleNextClick}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
 
+
+
+
+
+
+
+
+
+
 export default App;
+
+
---
+++ src/Page3.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Page3 = () => {
+  return <div className="font-bold text-4xl">Page3</div>;
+}
+
+export default Page3
